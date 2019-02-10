b1ef84eab6a85b48b12dd638998fbf0f2cbbc81c
Learner <linhdqtg@gmail.com>
2019-02-10 17:13:33 +0700

    Rebrand
    BTCM

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-        'api' => 'http://127.0.0.1:11898',
-        'blockTargetInterval' => 30,
-        'coinUnits' => 100
+        'api' => 'http://127.0.0.1:11358',
+        'blockTargetInterval' => 50,
+        'coinUnits' => 10000
 );
