55460a972b71f609aa1ecb6ddd4424e99c3362d0
Learner <linhdqtg@gmail.com>
2019-02-10 20:38:04 +0700

    Change http
    Paris Node

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://127.0.0.1:11358';
+var api = 'http://nodeparis.bitcoinmono.io:11358';
 var donationAddress = "";
 var blockTargetInterval = 50; // enter the block interval in seconds
 var coinUnits = 10000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
