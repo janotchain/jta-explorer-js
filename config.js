d7eb7b4c213a9eb76d8095e9d6534dbd313305bb
Learner <linhdqtg@gmail.com>
2019-02-11 20:03:08 +0700

    Fix API config
    Use different path

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'https://explorer.bitcoinmono.io/api';
+var api = 'https://explorer.bitcoinmono.io/daemon-api';
 var donationAddress = "";
 var blockTargetInterval = 50; // enter the block interval in seconds
 var coinUnits = 10000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
