584810adb463fc77bb50a6131cddf75f242267c1
Learner <linhdqtg@gmail.com>
2019-02-15 03:14:12 +0700

    Revert "make API great again" commit
    Great but does not work.

--- q/hashrate/index.php
+++ q/hashrate/index.php
@@ -1,10 +1,13 @@
-<?php
-require '../util.php';
-$config = (require '../../config.php');
-
-$info = fetch_getinfo($config['api']);
-
-$difficulty = $info['difficulty'];
-$hashrate = round($difficulty / $config['blockTargetInterval']);
-
+<?php 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, 'http://127.0.0.1:11358/getinfo');
+$result = curl_exec($ch);
+$obj = json_decode($result, TRUE);
+curl_close($ch);
+//print_r($obj);
+$difficulty = $obj['difficulty'];
+$hashrate = round($difficulty / 50);
 print_r($hashrate);
+?>
