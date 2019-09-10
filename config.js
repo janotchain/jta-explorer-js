9f10b9c0dd9cb960818a60afdb95fa5fe7fd5cbb
Learner <linhdqtg@gmail.com>
2019-09-10 16:29:06 +0700

    Ticker change
    BTCMZ

--- config.js
+++ config.js
@@ -3,18 +3,18 @@ var donationAddress = "";
 var blockTargetInterval = 50; // enter the block interval in seconds
 var coinUnits = 10000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
 var totalSupply =  10000000000000000; // enter the total supply in atomic units
-var symbol = 'btcm'; // enter the coin's ticker
+var symbol = 'BTCMZ'; // enter the coin's ticker
 var refreshDelay = 30000;
 
 // pools stats by MainCoins
 var networkStat = {
- "btcm": [
+ "BTCMZ": [
 	[""]
  ]
 };
 
 var networkStat2 = {
-    "btcm": [
+    "BTCMZ": [
 	["https://webbtcm.semipool.com","https://webbtcm.semipool.com/api"]
  ]
 };
