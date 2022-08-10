59609ee1ca69266bd562b91fefb765b8fe57950e
janotchain <onosmediang@gmail.com>
2022-08-10 12:55:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
-var api = 'https://explorer.bitcoinmono.io/daemon-api';
+var api = 'https://192.241.157.165:48006';
 var donationAddress = "";
 var blockTargetInterval = 50; // enter the block interval in seconds
 var coinUnits = 10000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
 var totalSupply =  10000000000000000; // enter the total supply in atomic units
-var symbol = 'BTCMZ'; // enter the coin's ticker
+var symbol = 'JTA'; // enter the coin's ticker
 var refreshDelay = 30000;
 
 // pools stats by MainCoins
 var networkStat = {
- "BTCMZ": [
+ "JTA": [
 	[""]
  ]
 };
 
 var networkStat2 = {
-    "BTCMZ": [
+    "JTA": [
 	["https://webbtcm.semipool.com","https://webbtcm.semipool.com/api"]
  ]
 };
